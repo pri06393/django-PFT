8ff4861732b5f19b90fe3fd710125be34c8d9e59
pri06393 <priyanka.sahoo@argusoft.in>
2025-08-07 10:15:24 +0530

    CRUD operations

--- finance/urls.py
+++ finance/urls.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from django.urls import path
-from finance.views import  RegisterView, DashboardView
+from finance.views import  RegisterView, DashboardView, TransactionCreationView, TransactionListView, updateTransaction, deleteTransaction
 from finance import views
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name = 'register'),
     path('', DashboardView.as_view(),name = 'Dashboard'),
+    path('transaction/add/', TransactionCreationView.as_view(),name = 'Transaction_add'),
+    path('transaction/', TransactionListView.as_view(),name = 'transaction_list'),
+    path('transaction/update/<str:pk>', views.updateTransaction,name = 'update_list'),
+    path('transaction/delete/<str:pk>', views.deleteTransaction,name = 'delete_list')
+
     
-    #path('login/', auth)
-    #path('register/dashboard',views.dashboard)
-    #path('login/', LoginView.as_view(), name = 'home'),
-    # path('', views.Home)
+    
+   
 ]
--- finance/views.py
+++ finance/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from django.views import View
-from finance.forms import RegistrationForm
+from finance.forms import RegistrationForm, TransactionForm
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Transaction
+from django.db.models import Sum
+
 
 class RegisterView(View):
     def get(self, request, *args, **kwargs):
@@ -17,31 +20,80 @@ class RegisterView(View):
             return redirect('Dashboard')
         return render(request, "finance/registration.html", {'form':form })
 
-# class LoginView(View):
+# class DashboardView(LoginRequiredMixin,View):
 #     def get(self, request, *args, **kwargs):
-#         form = LoginForm()
-       
-#         return render(request, "finance/login.html", {'form':form })
-#     def post(self, request, *args, **kwargs):
-#         form = LoginForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             login(request, user)
-#             return redirect('dashboard')
-#         return render(request, "finance/login.html", {'form':form })
-
-
-# def dashboard(request):
-#     return render(request, 'finance/dashboard.html')
-class DashboardView(LoginRequiredMixin,View):
-    def get(self, request, *args, **kwargs):
-        return render(request, "finance/dashboard.html")
+#         return render(request, "finance/dashboard.html")
     
-# class Home(View):
-#     def get(self,request,*args,**kwargs):
-#         return render(request, "finance/login.html")
+class TransactionCreationView(LoginRequiredMixin,View):
+    def get(self, request, *args, **kwargs):
+        form = TransactionForm()
+        return render(request, "finance/transaction.html", {'form':form })
+    def post(self, request, *args, **kwargs):
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit = False)
+            transaction.user = request.user
+            transaction.save()
+            return redirect('Dashboard')
+        return render(request, "finance/transaction.html", {'form':form })
+    
+class TransactionListView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        Transactions = Transaction.objects.all()
+        return render(request, "finance/transaction_list.html", {'Transactions': Transactions})
+    
+
+# def updateTransaction(request,pk):
+#     transaction = get_object_or_404(Transaction, id = pk)
+#    # Transactions = TransactionForm(instance=order)
+#     if request.method =='POST':
+#         form = TransactionForm(request.POST, instance = transaction)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('transaction_list')
+#         else:
+#             form = TransactionForm(instance=transaction)
+
+#     # context = {'Transactions':Transactions}
+#     return render(request, "finance/transaction.html",{'form':form})
+
+def updateTransaction(request, pk):
+    transaction = get_object_or_404(Transaction, id=pk)  # this avoids None
+    if request.method == 'POST':
+        form = TransactionForm(request.POST, instance=transaction)
+        if form.is_valid():
+            form.save()
+            return redirect('transaction_list')  # make sure this name exists in urls
+    else:
+        form = TransactionForm(instance=transaction)
+    return render(request, "finance/transaction.html", {'form': form})
 
 
 
-# def test(request):
-#     return HttpResponse("test")
+
+
+
+
+
+def deleteTransaction(request,pk):
+    transaction = get_object_or_404(Transaction, id = pk)
+    if request.method == 'POST':
+        transaction.delete()
+        return redirect('transaction_list')
+    return render(request, 'finance/deleteTransaction.html',{'item':transaction})
+
+
+class DashboardView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        transactions = Transaction.objects.filter(user=request.user).order_by('-date')
+        total_income = transactions.filter(transaction_type='Income').aggregate(Sum('amount'))['amount__sum'] or 0
+        total_expenses = transactions.filter(transaction_type='Expense').aggregate(Sum('amount'))['amount__sum'] or 0
+        balance = total_income - total_expenses
+        recent_transactions = transactions[:5]  # Show 5 recent
+        context = {
+            'total_income': total_income,
+            'total_expenses': total_expenses,
+            'balance': balance,
+            'recent_transactions': recent_transactions,
+        }
+        return render(request, "finance/dashboard.html", context)
